30092c4b8c8bf946cc17cd7bb4dc4eca88c36dbb
Jaye Popplewell <jlpopp01@gmail.com>
2021-05-03 18:25:01 -0400

    edited npm things

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,38 +5,38 @@ const mongoose = require('mongoose');
 router.use('/doc', function(req, res, next) {
     res.end(`Documentation http://expressjs.com/`);
   });
-  
+
   /**
    * Get a list of all files in the DB
    */
   router.get('/file', function(req, res, next) {
     const fileModel = mongoose.model('File');
-  
+
     fileModel.find({deleted: {$ne: true}}, function(err, files) {
       if (err) {
         console.log(err);
         return res.status(500).json(err);
       }
-    
+
       res.json(files);
     });
   });
-  
+
   /**
    * Get a single file by passing its id as a URL param
    */
   router.get('/file/:fileId', function(req, res, next) {
     const {fileId} = req.params;
     // same as 'const fileId = req.params.fileId'
-  
+
     const file = FILES.find(entry => entry.id === fileId);
     if (!file) {
       return res.status(404).end(`Could not find file '${fileId}'`);
     }
-  
+
     res.json(file);
   });
-  
+
   /**
    * Create a new file
    */
@@ -46,24 +46,24 @@ router.use('/doc', function(req, res, next) {
       email: req.body.email,
       description: req.body.description,
     };
-  
+
     File.create(fileData, function(err, newFile) {
       if (err) {
         console.log(err);
         return res.status(500).json(err);
       }
-  
+
       res.json(newFile);
     });
   });
-  
+
   /**
    * Update an existing file
    */
   router.put('/file/:fileId', function(req, res, next) {
     const File = mongoose.model('File');
     const fileId = req.params.fileId;
-  
+
     File.findById(fileId, function(err, file) {
       if (err) {
         console.error(err);
@@ -72,10 +72,10 @@ router.use('/doc', function(req, res, next) {
       if (!file) {
         return res.status(404).json({message: "File not found"});
       }
-  
+
       file.title = req.body.title;
       file.description = req.body.description;
-  
+
       file.save(function(err, savedFile) {
         if (err) {
           console.error(err);
@@ -83,18 +83,18 @@ router.use('/doc', function(req, res, next) {
         }
         res.json(savedFile);
       })
-  
+
     })
-  
+
   });
-  
+
   /**
    * Delete a file
    */
   router.delete('/file/:fileId', function(req, res, next) {
     const File = mongoose.model('File');
     const fileId = req.params.fileId;
-  
+
     File.findById(fileId, function(err, file) {
       if (err) {
         console.log(err);
@@ -103,16 +103,15 @@ router.use('/doc', function(req, res, next) {
       if (!file) {
         return res.status(404).json({message: "File not found"});
       }
-  
+
       file.deleted = true;
-  
+
       file.save(function(err, doomedFile) {
         res.json(doomedFile);
       })
-  
+
     })
   });
-  
-  
+
+
   module.exports = router;
-  
